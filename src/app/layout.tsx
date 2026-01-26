505b955c08b42d9cb5e4d8107add8edb1fe378ac
양정운 <yp071704@gmail.com>
2026-01-26 21:53:34 +0900

    feat: 로고 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,26 @@
 import { ReactNode } from "react";
 import "@/styles/globals.css";
+import { Metadata } from "next";
 
 type Props = {
   children: ReactNode;
+};
+
+export const metadata: Metadata = {
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/images/shathing.png",
+        type: "image/png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/images/shathing-dark.png",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 // Since we have a `not-found.tsx` page on the root, a layout file
