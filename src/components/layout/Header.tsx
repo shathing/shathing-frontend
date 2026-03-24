fc6b4c2701509492b93514465a6ee38ab4c2b7fb
양정운 <yp071704@gmail.com>
2026-03-24 15:58:47 +0900

    feat: 헤더 내비에 글쓰기 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -18,7 +18,10 @@ export default async function Header() {
         </Link>
         <nav>
           <ul className="flex gap-5">
-            {[{ href: "/share", title: t("share") }].map(({ href, title }, idx) => (
+            {[
+              { href: "/share", title: t("share") },
+              { href: "/share/post", title: t("post") },
+            ].map(({ href, title }, idx) => (
               <li key={idx}>
                 <Button asChild variant="ghost">
                   <Link href={href}>{title}</Link>
