80b28042c5d153f873366d675b7a28e5279c610e
양정운 <yp071704@gmail.com>
2026-03-24 14:27:49 +0900

    fix: prod 환경에서만 sentry init

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,16 +4,18 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://0857756500804e71b9225043553ecd54@o4511096870010880.ingest.us.sentry.io/4511096877481984",
+if (process.env.NODE_ENV == "production") {
+  Sentry.init({
+    dsn: "https://0857756500804e71b9225043553ecd54@o4511096870010880.ingest.us.sentry.io/4511096877481984",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
 
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-});
+    // Enable sending user PII (Personally Identifiable Information)
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+    sendDefaultPii: true,
+  });
+}
