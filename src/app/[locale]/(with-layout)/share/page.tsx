b35f54448624860d060f9eaeadff73bab4d0e370
양정운 <yp071704@gmail.com>
2026-03-25 12:51:31 +0900

    fix: 물건 목록 없을때 empty 보여주기

--- src/app/[locale]/(with-layout)/share/page.tsx
+++ src/app/[locale]/(with-layout)/share/page.tsx
@@ -3,27 +3,50 @@ import { shareApi } from "@/apis/share";
 import ShareItem from "./ShareItem";
 import ClientShareItemList from "./ClientShareItemList";
 import { getAgo } from "@/lib/getAgo";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyTitle } from "@/components/ui/empty";
+import { Button } from "@/components/ui/button";
+import { Link } from "@/i18n/navigation";
 
 export default async function Page() {
-  const { data } = await shareApi.getList();
+  let shareItems = null;
+
   const { ago } = await getAgo();
+
+  try {
+    const { data } = await shareApi.getList();
+    shareItems = data;
+  } catch {}
 
   return (
     <div className="space-y-2.5 my-2.5 relative">
       <SearchBar />
-      <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {data?.items.map((item) => (
-          <ShareItem
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            legalDong={item.legalDong}
-            photoUrls={item.photoUrls}
-            date={ago(item.createdDate)}
-          />
-        ))}
-        <ClientShareItemList />
-      </div>
+      {shareItems ? (
+        <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {shareItems?.items.map((item) => (
+            <ShareItem
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              legalDong={item.legalDong}
+              photoUrls={item.photoUrls}
+              date={ago(item.createdDate)}
+            />
+          ))}
+          <ClientShareItemList />
+        </div>
+      ) : (
+        <Empty>
+          <EmptyHeader>
+            <EmptyTitle>아직 공유된 물건이 없습니다.</EmptyTitle>
+            <EmptyDescription>직접 등록해 보는건 어떠신가요?</EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent>
+            <Button asChild>
+              <Link href="/share/post">등록하기</Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      )}
     </div>
   );
 }
