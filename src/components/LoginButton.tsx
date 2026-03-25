938f5f4b9de273c97dd550451238f9e2c6d8ddd4
양정운 <yp071704@gmail.com>
2026-03-25 23:49:45 +0900

    fix: 로그인 user 아이콘 CLS 향상 및 번역

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import useGetMe from "@/hooks/apis/useGetMe";
 import { Link, useRouter } from "@/i18n/navigation";
 import { useTranslations } from "next-intl";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -16,6 +15,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Spinner } from "@/components/ui/spinner";
 import { useQueryClient } from "@tanstack/react-query";
+import { User } from "lucide-react";
 
 export default function LoginButton({ isAuthenticated }: { isAuthenticated: boolean }) {
   const t = useTranslations("Header");
@@ -39,29 +39,31 @@ export default function LoginButton({ isAuthenticated }: { isAuthenticated: bool
     );
   }
 
-  if (isPending) return <Spinner />;
+  if (isPending)
+    return (
+      <div className="flex items-center justify-center size-9">
+        <Spinner />
+      </div>
+    );
 
   if (data) {
     return (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" size="icon" className="rounded-full">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" alt="shadcn" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <User />
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-32">
+        <DropdownMenuContent className="w-32" align="end">
           <DropdownMenuGroup>
             <DropdownMenuItem asChild className="cursor-pointer">
-              <Link href="/me">Profile</Link>
+              <Link href="/me">{t("profile")}</Link>
             </DropdownMenuItem>
           </DropdownMenuGroup>
           <DropdownMenuSeparator />
           <DropdownMenuGroup>
             <DropdownMenuItem variant="destructive" className="cursor-pointer" onClick={logout}>
-              Log out
+              {t("logout")}
             </DropdownMenuItem>
           </DropdownMenuGroup>
         </DropdownMenuContent>
