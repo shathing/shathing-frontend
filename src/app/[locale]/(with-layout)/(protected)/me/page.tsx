97a6d514e33b9247710933de3ebc1f5389faaa87
양정운 <yp071704@gmail.com>
2026-04-05 08:39:59 +0900

    fix: 내정보 페이지 높이 full 으로 변경

--- src/app/[locale]/(with-layout)/(protected)/me/page.tsx
+++ src/app/[locale]/(with-layout)/(protected)/me/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
   const username = data?.username ?? "-";
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex h-full w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <Card>
           <CardHeader>
