f10a9351dbaf65fbd4fc8b2e6023c88613b5e170
양정운 <yp071704@gmail.com>
2026-04-06 16:26:10 +0900

    fix: sitemap 경로에서 sitemap 이 중복되어 그룹처리

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
     },
     sitemap: [
       `${host}/sitemap.xml`,
-      ...(routing.locales.map((lnag) => `${host}/sitemap/share/sitemap/${lnag}.xml`
+      ...(routing.locales.map((lnag) => `${host}/share/sitemap/${lnag}.xml`
       ))
     ]
   }
