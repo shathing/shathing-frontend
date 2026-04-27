3cebd3a2aa59e38f3f62a2e58766d68a12d8839f
양정운 <yp071704@gmail.com>
2026-04-27 16:49:12 +0900

    feat: Google Analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,68 +1,89 @@
-import { host } from "@/config";
-import { Locale, hasLocale } from "next-intl";
-import { getTranslations, setRequestLocale } from "next-intl/server";
-import { routing } from "@/i18n/routing";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { hasLocale, Locale } from "next-intl";
+import { getTranslations, setRequestLocale } from "next-intl/server";
+import { host } from "@/config";
+import { routing } from "@/i18n/routing";
 import Providers from "@/providers";
 
-export async function generateMetadata(props: Omit<LayoutProps<"/[locale]">, "children">): Promise<Metadata> {
-  const { locale } = await props.params;
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-  const t = await getTranslations({
-    locale: locale as Locale,
-    namespace: "Common",
-  });
+export async function generateMetadata(
+	props: Omit<LayoutProps<"/[locale]">, "children">,
+): Promise<Metadata> {
+	const { locale } = await props.params;
 
-  const title = t("service-name");
-  const description = t("service-description");
-  const keywords = t("service-keywords");
+	const t = await getTranslations({
+		locale: locale as Locale,
+		namespace: "Common",
+	});
 
-  return {
-    metadataBase: host,
-    title: {
-      default: title,
-      template: `%s - ${title}`,
-    },
-    applicationName: title,
-    description,
-    keywords,
-    openGraph: {
-      title,
-      siteName: title,
-      description,
-      url: `${host}/${locale}`,
-      type: "website",
-      locale,
-      images: [
-        {
-          url: "/images/shathing.png",
-          alt: title,
-        },
-      ],
-    },
-  };
+	const title = t("service-name");
+	const description = t("service-description");
+	const keywords = t("service-keywords");
+
+	return {
+		metadataBase: host,
+		title: {
+			default: title,
+			template: `%s - ${title}`,
+		},
+		applicationName: title,
+		description,
+		keywords,
+		openGraph: {
+			title,
+			siteName: title,
+			description,
+			url: `${host}/${locale}`,
+			type: "website",
+			locale,
+			images: [
+				{
+					url: "/images/shathing.png",
+					alt: title,
+				},
+			],
+		},
+	};
 }
 
 export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }));
+	return routing.locales.map((locale) => ({ locale }));
 }
 
-export default async function LocaleLayout({ children, params }: LayoutProps<"/[locale]">) {
-  // Ensure that the incoming `locale` is valid
-  const { locale } = await params;
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
+export default async function LocaleLayout({
+	children,
+	params,
+}: LayoutProps<"/[locale]">) {
+	// Ensure that the incoming `locale` is valid
+	const { locale } = await params;
+	if (!hasLocale(routing.locales, locale)) {
+		notFound();
+	}
 
-  // Enable static rendering
-  setRequestLocale(locale);
+	// Enable static rendering
+	setRequestLocale(locale);
 
-  return (
-    <html lang={locale} suppressHydrationWarning>
-      <body>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang={locale} suppressHydrationWarning>
+			<head>
+				{/* <!-- Google tag (gtag.js) --> */}
+				<script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+				/>
+				<script>
+					{`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_ID}');`}
+				</script>
+			</head>
+			<body>
+				<Providers>{children}</Providers>
+			</body>
+		</html>
+	);
 }
